c475529ca3c87e8bce8c8968b347d6fdee950c9f
p00603582 <p00603582@huawei.com>
2026-02-16 16:15:32 +0800

    init qt proj

---
+++ mainwindow.h
@@ -0,0 +1,29 @@
+//
+// Created by huailiang on 2026/2/16.
+//
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    // 按钮点击的槽函数
+    void on_pushButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
